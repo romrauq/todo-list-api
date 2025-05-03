dcd75c9c024da861e60fa94882b6e574a86afc74
John Quarmor <johnequarmor@gmail.com>
2025-05-03 22:14:05 +0000

    Imported "Todo" model, Updated (all) routes to use MongoDB via Mongoose.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3500;
 const mongoose = require("mongoose");
 require("dotenv").config();
+const Todo = require("./models/Todo");
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -19,55 +20,78 @@ app.get("/", (req, res) => {
 });
 
 // GET all todos
-app.get("/todos", (req, res) => {
-	res.json(todos);
+app.get("/todos", async (req, res) => {
+	try {
+		const todos = await Todo.find();
+		res.json(todos);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 });
 
 // GET a single todo by ID
-app.get("/todos/:id", (req, res) => {
-	const id = parseInt(req.params.id);
-	const todo = todos.find((t) => t.id === id);
-	if (todo) {
+app.get("/todos/:id", async (req, res) => {
+	try {
+		const todo = await Todo.findById(req.params.id);
+		if (!todo) return res.status(404).json({ message: "Todo not found" });
 		res.json(todo);
-	} else {
-		res.status(404).json({ message: "Todo not found" });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
 	}
 });
 
 // POST a new todo
-app.post("/todos", (req, res) => {
+app.post("/todos", async (req, res) => {
 	const { title, completed } = req.body;
-	const newTodo = {
-		id: todos.length + 1,
+
+	if (!title) {
+		return res.status(400).json({ message: "Title is required" });
+	}
+
+	const newTodo = new Todo({
 		title,
 		completed: completed || false,
-	};
-	todos.push(newTodo);
-	res.status(201).json(newTodo);
+	});
+
+	try {
+		const savedTodo = await newTodo.save();
+		res.status(201).json(savedTodo);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 });
 
 // PUT (update) a todo by ID
-app.put("/todos/:id", (req, res) => {
-	const id = parseInt(req.params.id);
-	const index = todos.findIndex((t) => t.id === id);
-	if (index !== -1) {
-		const updatedTodo = { ...todos[index], ...req.body };
-		todos[index] = updatedTodo;
+app.put("/todos/:id", async (req, res) => {
+	try {
+		const updatedTodo = await Todo.findByIdAndUpdate(
+			req.params.id,
+			{ $set: req.body },
+			{ new: true, runValidators: true }
+		);
+
+		if (!updatedTodo) {
+			return res.status(404).json({ message: "Todo not found" });
+		}
+
 		res.json(updatedTodo);
-	} else {
-		res.status(404).json({ message: "Todo not found" });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
 	}
 });
 
 // DELETE a todo by ID
-app.delete("/todos/:id", (req, res) => {
-	const id = parseInt(req.params.id);
-	const index = todos.findIndex((t) => t.id === id);
-	if (index !== -1) {
-		const deletedTodo = todos.splice(index, 1);
-		res.json(deletedTodo[0]);
-	} else {
-		res.status(404).json({ message: "Todo not found" });
+app.delete("/todos/:id", async (req, res) => {
+	try {
+		const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
+
+		if (!deletedTodo) {
+			return res.status(404).json({ message: "Todo not found" });
+		}
+
+		res.json({ message: "Todo deleted", todo: deletedTodo });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
 	}
 });
 
